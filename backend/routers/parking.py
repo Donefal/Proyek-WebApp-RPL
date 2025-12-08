73a53e4c35392fd43f54c08380bd4027fe4cd21e
bagasyogta <bagas.yogta23@gmail.com>
2025-12-08 15:32:57 +0700

    update status lahan

--- backend/routers/parking.py
+++ backend/routers/parking.py
@@ -90,9 +90,10 @@ def get_spots(
     for slot in slots:
         # Check if slot is booked
         is_booked = slot.booked
-        is_occupied = slot.occupied
-        is_available = not is_booked and not is_occupied
-        
+       # is_occupied = slot.occupied --disable occupied
+       # is_available = not is_booked and not is_occupied
+        is_available = not is_booked
+
         # Get mikrokontroler info
         mikrokontroler = db.query(models.Mikrokontroler).filter(
             models.Mikrokontroler.id_mikrokontroler == slot.id_mikrokontroler
