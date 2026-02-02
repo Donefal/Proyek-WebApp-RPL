997bd06908b76b514fa37fd1b45828b7cd5d3b49
bagasyogta <bagasyogta223@gmail.com>
2026-02-02 15:26:52 +0700

    test 2

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DB_USER = "web"
 DB_PASSWORD = ""     # atau password MySQL kamu
-DB_HOST = "192.168.4.13:8081"
+DB_HOST = "192.168.4.13"
+DB_PORT = "3306"
 DB_NAME = "fastapi_db"
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
