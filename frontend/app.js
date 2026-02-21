0b6cfc56ff39d65ec88d28ff1581eb9c3e5d70e7
bagasyogta <bagasyogta223@gmail.com>
2026-02-21 22:10:01 +0700

    api fast 5

--- frontend/app.js
+++ frontend/app.js
@@ -5,7 +5,7 @@ const getApiBaseUrl = () => {
   const hostname = window.location.hostname;
   // If accessing from localhost, use localhost:8000
   if (hostname === 'localhost' || hostname === '127.0.0.1') {
-    return 'https://fastapi.saikeno.space';
+    return 'http://192.168.4.13:3001';
   }
   // Otherwise, use the same hostname with port 8000
   return `https://fastapi.saikeno.space`;
